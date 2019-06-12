65968e04467704341dcd70d2e835a1d64f6270c7
Rachel Tezza <racheletezza@gmail.com>
2019-06-12 16:35:30 -0400

    objects reorganized

--- script.js
+++ script.js
@@ -1,97 +1,137 @@
 // make each catgory into an object that holds values for the points, questions, and answers
-
+console.log("Connected")
 let htmlCategory = {
-    pointValue: [100 {
+    pointOption100: { 
+                    pointValue: 100,
                     question: "what does HTML stand for?",
                     answer: "hypertext markup language"
-                }, 
-                200 {
+    }, 
+    pointOption200: {
+                    pointValue: 200,
                     question: "what is the element tag for heading 1?",
                     answer: "<h1>"
-                }, 
-                300 {
+    }, 
+    pointOption300: {
+                    pointValue: 300,
                     question: "how would you make a div tag with the class 'trees'?",
                     answer: "<div class='trees'>"
-                }, 
-                400 {
+    }, 
+    pointOption400: {
+                    pointValue: 400,
                     question: "what should every HTML document start with?",
                     answer: "<!DOCTYPE html>"
-                }, 
-                500 {
+    }, 
+    pointOption500: {
+                    pointValue: 500,
                     question: "what is the child element tag of an unordered list?",
                     answer: "<li>"
-                }]
+    }
 
 }
 
 let cssCategory = {
-    pointValue: [100 {
+    pointOption100: {
+                    pointValue: 100 
                     question: "what does the 'C' in CSS stand for?",
                     answer: "cascading"
-                }, 
-                200 {
+    }, 
+    pointOption200: {
+                    pointValue: 200 
                     question: "how would you make a font color 'pink' (as a string)?",
                     answer: "color: pink;"
-                }, 
-                300 {
+    }, 
+    pointOption300:
+                    {
+                    pointValue: 300
                     question: "how would you denote the css selector for the class 'movies'?",
                     answer: ".movies"
-                }, 
-                400 {
+    }, 
+    pointOption400:
+                    {
+                    pointValue: 400
                     question: "how would you denote the css selector for the id 'ferrari'?",
                     answer: "<#ferrari>"
-                }, 
-                500 {
+    }, 
+    pointOption500:
+                    {
+                    pointValue: 500
                     question: "tag would you write to link the css stylesheet styles.css to your HTML?",
                     answer: "<link rel="stylesheet" href="styles.css">"
-                }]
+    }
 
 }
 
 let javascriptCategory = {
-    pointValue: [100 {
+    pointOption100:            
+                    {
+                    pointValue: 100 
                     question: "what is another term for 'plain' javascript?",
                     answer: "vanilla"
-                }, 
-                200 {
+    }, 
+    pointOption200:
+                    {
+                    pointValue: 200
                     question: "how would you write the code to link the javascript file called 'script.js' to your html file?",
                     answer: "<script src="script.js"></script>"
-                }, 
-                300 {
+    }, 
+    pointOption300:
+                    {
+                    pointValue: 300
                     question: "what is the first word you use to access an element in the DOM?",
                     answer: "document"
-                }, 
-                400 {
+    }, 
+    pointOption400:
+                    {
+                    pointValue: 400
                     question: "what does API stand for?",
                     answer: "application program interface"
-                }, 
-                500 {
+    }, 
+    pointOption500:
+                    {
+                    pointValue: 500
                     question: "what do you call a function inside of an object?",
                     answer: "method"
-                }]
+    }
 
 }
 
 let animalsCategory = {
-    pointValue: [100 {
+    pointOption100: 
+                    {
+                    pointValue: 100
                     question: "what continent do kangaroos live on?",
                     answer: "australia"
-                }, 
-                200 {
+    }, 
+    pointOption200:
+                    {
+                    pointValue: 200
                     question: "how many legs does a crab have?",
                     answer: "10"
-                }, 
-                300 {
+    }, 
+    pointOption300:
+                    {
+                    pointValue: 300
                     question: "what do you call a large group of lions?",
                     answer: "pride"
-                }, 
-                400 {
+    }, 
+    pointOption400:
+                    {
+                    pointValue: 400
                     question: "what do you call sheep's hair?",
                     answer: "wool"
-                }, 
-                500 {
+    }, 
+    pointOption500:
+                    {
+                    pointValue: 500
                     question: "what is the only animal that can't jump?",
                     answer: "elephant"
-                }]
+    }
 
 }
+// // set variable for the 100 point
+// let htmlFor100 = document.querySelector('#HTML100')
+// htmlFor100.addEventListener('click', flipToQuestion)
+// function flipToQuestion() {
+//     htmlFor100.textContent = animalsCategory.pointValue[0][0];
+//     console.log(htmlFor100.textContent);
+// }
