7e71062a27c6239876f904abd0cb0a0a629b3247
Rachel Tezza <racheletezza@gmail.com>
2019-06-12 15:59:23 -0400

    script updates made categories into objects

--- script.js
+++ script.js
@@ -1,23 +1,97 @@
-let HtmlCategory = {
+// make each catgory into an object that holds values for the points, questions, and answers
+
+let htmlCategory = {
     pointValue: [100 {
-                    question: "what does HTML stand for?"
+                    question: "what does HTML stand for?",
                     answer: "hypertext markup language"
                 }, 
                 200 {
-                    question: "what does HTML stand for?"
-                    answer: "hypertext markup language"
+                    question: "what is the element tag for heading 1?",
+                    answer: "<h1>"
                 }, 
                 300 {
-                    question: "what does HTML stand for?"
-                    answer: "hypertext markup language"
+                    question: "how would you make a div tag with the class 'trees'?",
+                    answer: "<div class='trees'>"
                 }, 
                 400 {
-                    question: "what does HTML stand for?"
-                    answer: "hypertext markup language"
+                    question: "what should every HTML document start with?",
+                    answer: "<!DOCTYPE html>"
                 }, 
                 500 {
-                    question: "what does HTML stand for?"
-                    answer: "hypertext markup language"
+                    question: "what is the child element tag of an unordered list?",
+                    answer: "<li>"
+                }]
+
+}
+
+let cssCategory = {
+    pointValue: [100 {
+                    question: "what does the 'C' in CSS stand for?",
+                    answer: "cascading"
+                }, 
+                200 {
+                    question: "how would you make a font color 'pink' (as a string)?",
+                    answer: "color: pink;"
+                }, 
+                300 {
+                    question: "how would you denote the css selector for the class 'movies'?",
+                    answer: ".movies"
+                }, 
+                400 {
+                    question: "how would you denote the css selector for the id 'ferrari'?",
+                    answer: "<#ferrari>"
+                }, 
+                500 {
+                    question: "tag would you write to link the css stylesheet styles.css to your HTML?",
+                    answer: "<link rel="stylesheet" href="styles.css">"
+                }]
+
+}
+
+let javascriptCategory = {
+    pointValue: [100 {
+                    question: "what is another term for 'plain' javascript?",
+                    answer: "vanilla"
+                }, 
+                200 {
+                    question: "how would you write the code to link the javascript file called 'script.js' to your html file?",
+                    answer: "<script src="script.js"></script>"
+                }, 
+                300 {
+                    question: "what is the first word you use to access an element in the DOM?",
+                    answer: "document"
+                }, 
+                400 {
+                    question: "what does API stand for?",
+                    answer: "application program interface"
+                }, 
+                500 {
+                    question: "what do you call a function inside of an object?",
+                    answer: "method"
+                }]
+
+}
+
+let animalsCategory = {
+    pointValue: [100 {
+                    question: "what continent do kangaroos live on?",
+                    answer: "australia"
+                }, 
+                200 {
+                    question: "how many legs does a crab have?",
+                    answer: "10"
+                }, 
+                300 {
+                    question: "what do you call a large group of lions?",
+                    answer: "pride"
+                }, 
+                400 {
+                    question: "what do you call sheep's hair?",
+                    answer: "wool"
+                }, 
+                500 {
+                    question: "what is the only animal that can't jump?",
+                    answer: "elephant"
                 }]
 
 }
