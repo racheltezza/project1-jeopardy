371aefbc010215be57809a7f92234633d777bbf3
Rachel Tezza <racheletezza@gmail.com>
2019-06-12 12:32:36 -0400

    more styles and script beginnings html object made

---
+++ script.js
@@ -0,0 +1,23 @@
+let HtmlCategory = {
+    pointValue: [100 {
+                    question: "what does HTML stand for?"
+                    answer: "hypertext markup language"
+                }, 
+                200 {
+                    question: "what does HTML stand for?"
+                    answer: "hypertext markup language"
+                }, 
+                300 {
+                    question: "what does HTML stand for?"
+                    answer: "hypertext markup language"
+                }, 
+                400 {
+                    question: "what does HTML stand for?"
+                    answer: "hypertext markup language"
+                }, 
+                500 {
+                    question: "what does HTML stand for?"
+                    answer: "hypertext markup language"
+                }]
+
+}
