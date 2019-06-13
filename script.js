23155ce91289bf28de9aaa9b2497a435be764aa9
Rachel Tezza <racheletezza@gmail.com>
2019-06-12 20:38:07 -0400

    flip to question function for html for 100

--- script.js
+++ script.js
@@ -31,32 +31,32 @@ let htmlCategory = {
 
 let cssCategory = {
     pointOption100: {
-                    pointValue: 100 
+                    pointValue: 100, 
                     question: "what does the 'C' in CSS stand for?",
                     answer: "cascading"
     }, 
     pointOption200: {
-                    pointValue: 200 
+                    pointValue: 200, 
                     question: "how would you make a font color 'pink' (as a string)?",
                     answer: "color: pink;"
     }, 
     pointOption300:
                     {
-                    pointValue: 300
+                    pointValue: 300,
                     question: "how would you denote the css selector for the class 'movies'?",
                     answer: ".movies"
     }, 
     pointOption400:
                     {
-                    pointValue: 400
+                    pointValue: 400,
                     question: "how would you denote the css selector for the id 'ferrari'?",
                     answer: "<#ferrari>"
     }, 
     pointOption500:
                     {
-                    pointValue: 500
+                    pointValue: 500,
                     question: "tag would you write to link the css stylesheet styles.css to your HTML?",
-                    answer: "<link rel="stylesheet" href="styles.css">"
+                    answer: "<link rel='stylesheet' href='styles.css'>"
     }
 
 }
@@ -64,31 +64,31 @@ let cssCategory = {
 let javascriptCategory = {
     pointOption100:            
                     {
-                    pointValue: 100 
+                    pointValue: 100, 
                     question: "what is another term for 'plain' javascript?",
                     answer: "vanilla"
     }, 
     pointOption200:
                     {
-                    pointValue: 200
+                    pointValue: 200,
                     question: "how would you write the code to link the javascript file called 'script.js' to your html file?",
-                    answer: "<script src="script.js"></script>"
+                    answer: "<script src='script.js'></script>"
     }, 
     pointOption300:
                     {
-                    pointValue: 300
+                    pointValue: 300,
                     question: "what is the first word you use to access an element in the DOM?",
                     answer: "document"
     }, 
     pointOption400:
                     {
-                    pointValue: 400
+                    pointValue: 400,
                     question: "what does API stand for?",
                     answer: "application program interface"
     }, 
     pointOption500:
                     {
-                    pointValue: 500
+                    pointValue: 500,
                     question: "what do you call a function inside of an object?",
                     answer: "method"
     }
@@ -98,40 +98,41 @@ let javascriptCategory = {
 let animalsCategory = {
     pointOption100: 
                     {
-                    pointValue: 100
+                    pointValue: 100,
                     question: "what continent do kangaroos live on?",
                     answer: "australia"
     }, 
     pointOption200:
                     {
-                    pointValue: 200
+                    pointValue: 200,
                     question: "how many legs does a crab have?",
                     answer: "10"
     }, 
     pointOption300:
                     {
-                    pointValue: 300
+                    pointValue: 300,
                     question: "what do you call a large group of lions?",
                     answer: "pride"
     }, 
     pointOption400:
                     {
-                    pointValue: 400
+                    pointValue: 400,
                     question: "what do you call sheep's hair?",
                     answer: "wool"
     }, 
     pointOption500:
                     {
-                    pointValue: 500
+                    pointValue: 500,
                     question: "what is the only animal that can't jump?",
                     answer: "elephant"
     }
 
 }
-// // set variable for the 100 point
-// let htmlFor100 = document.querySelector('#HTML100')
-// htmlFor100.addEventListener('click', flipToQuestion)
-// function flipToQuestion() {
-//     htmlFor100.textContent = animalsCategory.pointValue[0][0];
-//     console.log(htmlFor100.textContent);
-// }
+// set variable for the 100 point square in the html category
+let htmlFor100 = document.querySelector('#HTML100')
+// create event that changes the text content from points to the question when clicked
+htmlFor100.addEventListener('click', flipToQuestion)
+function flipToQuestion() {
+    htmlFor100.textContent = htmlCategory.pointOption100.question;
+    console.log(htmlFor100.textContent);
+}
